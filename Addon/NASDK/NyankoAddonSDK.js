9caf783ae95fc95b5d0a7703c4757389281f36f3
nyantorusabu <nyantorusabu2@outlook.jp>
2026-01-15 22:20:43 +0000

    UPD

--- Addon/NASDK/NyankoAddonSDK.js
+++ Addon/NASDK/NyankoAddonSDK.js
@@ -66,9 +66,9 @@
 				),
 				{
 					menuIconURI:
-						'https://nyantorusabu.github.io/Re-013/Addon/NASDK/Icon.svg',
+						'https://nyantorusabu.github.io/Re-013/Addon/NASDK/Menu.svg',
 					blockIconURI:
-						'https://nyantorusabu.github.io/Re-013/Addon/NASDK/Icon.svg',
+						'https://nyantorusabu.github.io/Re-013/Addon/NASDK/Block.svg',
 					color1: '#ffad87',
 					color2: '#ff9c6e',
 					color3: '#ff8a54',
