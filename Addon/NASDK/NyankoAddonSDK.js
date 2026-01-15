a558c96f889f9484396b4fef6774b8f7c8491dbc
nyantorusabu <nyantorusabu2@outlook.jp>
2026-01-15 22:17:18 +0000

    UPD

--- Addon/NASDK/NyankoAddonSDK.js
+++ Addon/NASDK/NyankoAddonSDK.js
@@ -63,7 +63,16 @@
 						'RLVar [ID]',
 						arg('ID', 'S', 'GMain')
 					)
-				)
+				),
+				{
+					menuIconURI:
+						'https://nyantorusabu.github.io/Re-013/Addon/NASDK/Icon.svg',
+					blockIconURI:
+						'https://nyantorusabu.github.io/Re-013/Addon/NASDK/Icon.svg',
+					color1: '#ffad87',
+					color2: '#ff9c6e',
+					color3: '#ff8a54',
+				}
 			);
 		}
 
